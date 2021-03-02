bfd354a304ea4f9acccafef9099992271fd5361c
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-02 17:05:52 +0100

    Create Puzzle entity

---
+++ src/Entity/Puzzle.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PuzzleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PuzzleRepository::class)
+ */
+class Puzzle
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sentence;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSentence(): ?string
+    {
+        return $this->sentence;
+    }
+
+    public function setSentence(string $sentence): self
+    {
+        $this->sentence = $sentence;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
