6b6123ee228bf3de73a0cbdeb5cf630e2c859049
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-02 19:55:54 +0100

    Add constructor in PuzzleController

--- src/Controller/PuzzleController.php
+++ src/Controller/PuzzleController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Puzzle;
+use App\Repository\PuzzleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
- *   * @Route("/api", name="puzzle_api")
+ *   * @Route("/api")
  */
 class PuzzleController extends AbstractController
 {
-    #[Route('/puzzle', name: 'puzzle')]
-    public function index(): Response
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var PuzzleRepository
+     */
+    private $puzzleRepository;
+
+
+    public function __construct(EntityManagerInterface $entityManager, PuzzleRepository $puzzleRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->puzzleRepository = $puzzleRepository;
+    }
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
