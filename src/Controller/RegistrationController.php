77d035492651f3b02b10940a49db4a98566d4f3c
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-04 11:33:00 +0100

    Create Registration Controller

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Context\Context;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->userRepository = $userRepository;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $email = $request->query->get('email');
+        $password = $request->query->get('password');
+
+        $user = $this->userRepository->findOneBy(['email' => $email,]);
+
+        if (!is_null($user)) {
+            return new JsonResponse(['message' => 'User already exists'], Response::HTTP_CONFLICT);
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Created'], Response::HTTP_CREATED);
+    }
+}
