00b88cb57d927b4c1ecadf51760df8b12dd1142f
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-16 09:48:47 +0100

    Rename RegistrationController to AuthController

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class RegistrationController extends AbstractController
+class AuthController extends AbstractController
 {
 
     /**
