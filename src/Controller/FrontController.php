7c0563af7893ce011abf29e89f50a222f1aa737d
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-04-09 00:00:00 +0924

    Add new route to homepage with redirection

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+  /**
+   * @Route("/", name="main_page")
+   */
+  public function index(): Response
+  {
+
+    return $this->redirectToRoute('app.swagger_ui');
+  }
+}
