2f90f15012d47769270aaf21a101455aed4d4274
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-04-10 00:00:00 +0659

    Add event listener to 403 and 404 HTTP exceptions

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+
+        if ($_ENV['APP_ENV'] == 'prod') {
+            return;
+        }
+
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof AccessDeniedHttpException) {
+            $event->setResponse(new JsonResponse('Not enough permission', Response::HTTP_FORBIDDEN));
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            $event->setResponse(new JsonResponse('No Route Found', Response::HTTP_NOT_FOUND));
+        }
+    }
+}
